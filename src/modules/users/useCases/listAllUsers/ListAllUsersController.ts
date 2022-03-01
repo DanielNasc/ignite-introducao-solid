3599837a70044cd73032edd12409878917c582fd
DanielNasc <danielnasc15987@gmail.com>
2022-03-01 18:18:15 -0300

    finished

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,18 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.headers;
+
+    if (Array.isArray(user_id)) {
+      return response.status(404).json({ error: "X -> String[]  V -> String" });
+    }
+
+    try {
+      const list = this.listAllUsersUseCase.execute({ user_id });
+      return response.json(list);
+    } catch (e) {
+      return response.status(400).json({ error: e });
+    }
   }
 }
 
